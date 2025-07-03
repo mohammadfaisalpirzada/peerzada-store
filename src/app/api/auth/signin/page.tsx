b33d2d1f57b3d194cccd8c2c9269046deaa1fdc7
Muhammad Faisal Peerzada <mohammadfaisalpirzada@gmail.com>
2025-07-03 18:46:44 +0500

    Version2g

--- src/app/api/auth/signin/page.tsx
+++
@@ -1,3 +0,0 @@
-// This file is intentionally left blank to prevent Next.js from treating this as a route.
-// ok this is all
-export {};
--- src/app/auth/signin/page.tsx
+++
@@ -1,7 +0,0 @@
-// src/app/auth/signin/page.tsx
-"use client";
-import LoginForm from "../../../components/login/LoginForm";
-
-export default function SignInPage() {
-  return <LoginForm />;
-}
